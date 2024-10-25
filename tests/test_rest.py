4d83ef99d870903edf2ef2b3e90bbbea8be887c2
Erik <erik@montnemery.com>
2024-10-25 11:54:45 +0200

    Silence ruff

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -100,7 +100,7 @@ async def test_version_supported(
     responses: aioresponses,
     rest_client: Go2RtcRestClient,
     server_version: str,
-    expected_result: bool,
+    expected_result: bool,  # noqa: FBT001
 ) -> None:
     """Test webrtc offer."""
     payload = json.loads(load_fixture("application_info_answer.json"))
